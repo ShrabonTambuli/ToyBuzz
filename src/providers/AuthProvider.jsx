3c04e4c18daf2486b624ef3c2edb0d47194ebf60
ShrabonTambuli <shrabonsa@gmail.com>
2023-05-21 20:14:10 +0600

    update provider code

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -41,7 +41,6 @@ const AuthProvider = ({children}) => {
     useEffect(()=>{
         const unsubscribe = onAuthStateChanged(auth, currentUser =>{
             setUser(currentUser);
-            console.log(currentUser);
             setLoading(false);
         })
         return () =>{
