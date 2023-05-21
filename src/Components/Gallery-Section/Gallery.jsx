f37170d8157b7a0a1f57b29bb99e7f92cb541038
ShrabonTambuli <shrabonsa@gmail.com>
2023-05-21 17:36:03 +0600

    update gallery section

--- src/Components/Gallery-Section/Gallery.jsx
+++ src/Components/Gallery-Section/Gallery.jsx
@@ -1,6 +1,7 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
+import './Gallery.css'
 
 const Gallery = () => {
 
@@ -9,7 +10,7 @@ const Gallery = () => {
       }, []);
 
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-y-5 mt-14 mx-16 container">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-y-5 mt-14 mx-16 container mar">
             <div className="card w-80 bg-base-100 shadow-xl image-full h-60 mx-auto" data-aos="fade-right">
                 <figure><img src="/images/G-1.jpg" alt="Shoes" /></figure>
                 <div className="card-body">
